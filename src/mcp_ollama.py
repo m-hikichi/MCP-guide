79d8ab7f1503ba72a27438438899bb99ab62a59d
m_hikichi <hijiki.mshr19980119@outlook.com>
2025-05-06 10:39:52 +0900

    feat: LLMでMCPサーバを用いたサンプルコードの追加

---
+++ src/mcp_ollama.py
@@ -0,0 +1,53 @@
+import asyncio
+import json
+
+from langchain_core.messages import SystemMessage
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_ollama import ChatOllama
+from langgraph.prebuilt import create_react_agent
+
+
+def load_mcp_config(path: str) -> dict[str, any]:
+    """
+    指定されたJSONファイルからMCPサーバー設定を読み込む
+
+    Args:
+        path (str): 設定ファイルのパス
+
+    Returns:
+        dict[str, any]: MCPサーバーの設定情報
+    """
+    with open(path, "r", encoding="utf-8") as file:
+        return json.load(file).get("mcpServers", {})
+
+
+async def run_agent_query():
+    # LLM (ollama) の初期化
+    llm = ChatOllama(
+        base_url="http://ollama:11434",
+        model="Llama-3-ELYZA:8B-q4_K_M",
+        temperature=0,
+    )
+
+    # MCPサーバの設定読み込み
+    config = load_mcp_config("mcp_config.json")
+
+    # MCPクライアントとReActエージェントのセットアップ
+    async with MultiServerMCPClient(config) as client:
+        system_prompt = SystemMessage(
+            "あなたはMCPサーバーを使用するAIアシスタントです。"
+            "Toolの結果を優先して回答として採用してください"
+            "回答は日本語でお願いします。"
+        )
+        agent = create_react_agent(llm, client.get_tools(), prompt=system_prompt)
+
+        # エージェントにクエリを送信し、結果を出力
+        result = await agent.ainvoke({"messages": "123 * 111は?"})
+
+        for message in result["messages"]:
+            print(f"\n--- {type(message).__name__} ---")
+            print(message)
+
+
+if __name__ == "__main__":
+    asyncio.run(run_agent_query())
--- src/mcp_server.py
+++ src/mcp_server.py
@@ -10,5 +10,21 @@ def hello_world(name: str) -> str:
     return f"Hello, {name}!"
 
 
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """
+    2つの値を受け取り、その和を返す関数。
+    """
+    return a + b
+
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """
+    2つの値を受け取り、その積を返す関数。
+    """
+    return a * b
+
+
 if __name__ == "__main__":
     mcp.run()
